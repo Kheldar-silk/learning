f8edef58b3a514991af3518aad97a2e28421ff20
AMAR Jp <amarjp13@gmail.com>
2020-04-13 13:15:20 +0200

    init project

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+	<h1>Test H1</h1>
+	<?php
+
+
+	// Chiffres de la contamination par pays
+			$monde = 1709014;
+			$sate = 505599;
+			$spain = 161852;
+			$italy = 147577;
+			$germany = 122627;
+			$france = 90676;
+			$china = 81953;
+			$uk = 73758;
+			$iran = 70029;
+			$turqey = 47029;
+			$belgium = 28018;
+			$suisse = 24713;
+			$holland = 24413;
+			$canada = 22148;
+			$brasil = 19943;
+			$portugal = 15987;
+			$autriche = 13744;
+			$russia = 13584;
+			$israel = 10505;
+
+			if ($france > 50000) {
+				echo "Faut acheter des masques bordel";
+			}
+
+
+
+	?>
+
+
+</body>
+</head>
+</html>
